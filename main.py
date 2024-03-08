46f69cb61aef61192302a9809c8711e882e7ce04
Satyam Kumar <isatyamks@gmail.com>
2024-03-09 01:48:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 import tweepy
 import time
 
-access_token="1588896639149711367-6NPpdTuIZTYab1ZFaVR2C2IwHbPd5M"
-access_token_secret="Mt6hGciXjxB91COOXrboVdwR8Y08ttJPH7LpuCIAOu5Kd"
-bearer_token = r"AAAAAAAAAAAAAAAAAAAAAIvSsgEAAAAA3XEBk2LMAj%2BuI7MGP7i%2BamdSTAc%3DIIxDW6KiE03Mug9kArQsVTgZuoCnITjwhJShfNGHSDqKcV9J3I"
-api_key = "NSqapwvva60OiwCGFyjECwMYD"
-api_secret ="bH3UY5CzrB1LWnIC51nhNk1zEALvF7FNf4izFxLaf25PnNiGZD"
-client_id = "M0ZhTWt3elFIckp3Q3d4bm5OWlI6MTpjaQ"
-client_secret = "qoFciMpVz_DX29DXKlMK0_nyXwdEhx39p0ptClIJATq_F1Jjfb"
+# access_token="158889667uIZ3136-6NPpdT9hshs14971TYab1ZBhsbFaVR2C2IwHbPd5M"
+# access_token_secret="Mt6sgsgshghGcigXjxB91COOXrboVdwR8Y08ttJPjsbvvH7LpuCIAOu5Kd"
+# bearer_token = r"s sjbvsEBk2LMAj%2BuI7MGP7i%2BamdSTAc%3DIIxDW6KiE03Mug9kArQsVTgZuoCnITjwhJShfNGHSDqKcV9J3I"
+# api_key = "NSqahspwhsvva60OiwhsCGFhsyjECwMYD"
+# api_secret ="bH3UY5CzrB1LWnIC51ngsgsghNk1zEALvFsj vjks7FNf4izFxLaf25PnNiGZD"
+# client_id = "M0ZhTWt3elFIckp3Qfsns48u4bm5OWlI6MTpjaQ"
+# client_secret = "qoFciMpVz_DX29DXKsjfigjHUFHUSshsmmkskngXwdEhx39p0ptClIJATq_F1Jjfb"  ----------------------------------$manipulated (dont use this one)
 
 client = tweepy.Client(bearer_token,api_key,api_secret,access_token,access_token_secret)
 auth= tweepy.OAuth1UserHandler(api_key,api_secret,access_token,access_token_secret)
